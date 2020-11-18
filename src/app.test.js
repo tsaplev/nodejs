7b73966bc78537c982094a439badb632859c9d84
Alexander Tsaplev <alexander@tsaplev.me>
2020-11-18 15:57:53 +0300

    Migrate tests from folder to files

--- src/app.test.js
+++ src/app.test.js
@@ -1,14 +1,14 @@
-const { sum } = require("../src/app");
+const { sum } = require("./app");
 
 describe("App functions", () => {
   test("it should sum all function arguments", () => {
     const firstSummand = 2;
     const secondSummand = 5;
     const thirdSummand = 0;
-    const expectedResult = 7;
+    const expected = 7;
 
     const result = sum(firstSummand, secondSummand, thirdSummand);
 
-    expect(result).toEqual(expectedResult);
+    expect(result).toEqual(expected);
   });
 });
